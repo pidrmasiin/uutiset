e2530c0587ff3b91539736bc4f9c9c4fd2b0ac21
pidrmasiin <petteri.paakkonen@helsinki.fi>
2017-12-11 21:40:30 +0200

    tests

--- src/test/java/wad/StatusTest.java
+++ src/test/java/wad/StatusTest.java
@@ -5,10 +5,39 @@
  */
 package wad;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 /**
  *
  * @author petteri
  */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class StatusTest {
+    @Autowired
+    private WebApplicationContext webAppContext;
+
+    private MockMvc mockMvc;
     
+    @Before
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webAppContext).build();
+    }
+    
+    @Test
+    public void statusOk() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk());
+    }
 }
